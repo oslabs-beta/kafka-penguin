8eaa1ab9d679f18d546263b777dcb37f6fddb9a6
Kushal Talele <kushal.talele@gmail.com>
2021-03-23 20:37:35 -0400

    consumer folder, consumer, client, test TS files

--- consumer/client.ts
+++ consumer/client.ts
@@ -1,16 +1,15 @@
-
 import {Kafka} from 'kafkajs'
 require('dotenv').config();
 
 export default new Kafka({
-    'clientId': 'myapp',
-    'brokers': [process.env.KAFKA_BOOTSTRAP_SERVER],
+    clientId: 'myapp',
+    brokers: [process.env.KAFKA_BOOTSTRAP_SERVER],
     ssl: true,
     sasl: {
       mechanism: 'plain',
       username: process.env.KAFKA_USERNAME,
       password: process.env.KAFKA_PASSWORD
-    } 
+    }     
   })
-    .admin();
 
+  
--- consumer/consumer.ts
+++ consumer/consumer.ts
@@ -1,50 +1,42 @@
 const {Kafka}= require('kafkajs')
 require('dotenv').config();
+import kafka from './client'
 
-run();
+const consumer = kafka.consumer({groupId: 'test'});
 
-async function run() {
-  try {
-    const kafka = new Kafka({
-      clientId: 'myapp',
-      brokers: [process.env.KAFKA_BOOTSTRAP_SERVER],
-      ssl: true,
-      sasl: {
-        mechanism: 'plain',
-        username: process.env.KAFKA_USERNAME,
-        password: process.env.KAFKA_PASSWORD
-      }     
-    })
-
-    const consumer = kafka.consumer({groupId: 'test'});
-    console.log('connecting...')
-
-    await consumer.connect()
-    console.log('connected!');
-
-    await consumer.subscribe({
-      topic: 'test-topic',
-      //When fromBeginning is true, the group will use the earliest offset.
-      // If set to false, it will use the latest offset. The default is false.
-      fromBeginning: false, 
-    }) 
-
-    interface ResultObj {
-      message: {value: string;}
-      partition: number
-    }
-
-    await consumer.run({
-
-      eachMessage: async  (result:ResultObj) => {    
-        console.log(`Received message ${result.message.value} on partition ${result.partition}`)
-      }
-    })
-  } catch (ex) {
-    console.error(ex)
-  }
-  finally {
-    // process.exit(0);  
-  }
+const consumerConnect = async () => {
+  await consumer.connect()
+  console.log('Connected')
 }
+
+const consumerSubscribe = async () => {
+  await consumer.subscribe({
+    topic: 'test-topic',
+    //When fromBeginning is true, the group will use the earliest offset.
+    // If set to false, it will use the latest offset. The default is false.
+    fromBeginning: false, 
+  }) 
+}
+
+const consumerDisconnect = async () => {
+  await consumer.disconnect();
+  console.log('Disconnected')
+}
+
+const consumerRun = async () => {
+  await consumer.run({
+    eachMessage: async  (result: { message: { value: any; }; partition: any; }) => {    
+      console.log(`Received message ${result.message.value} on partition ${result.partition}`)
+    }
+  })
+}
+//executing function calls
+consumerConnect()
+  .then(() => consumerSubscribe())
+  .then(() => consumerRun())
+  .then(() => consumerDisconnect())  
+  .catch(error => {
+    console.log('Error in consumer: ', error)
+  })  
+  .finally(() => process.exit(0))
 

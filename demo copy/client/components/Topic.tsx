28ed6c96c1f015bc12d272ce4f02988f7fa1ad22
Timeo <timwillie73@gmail.com>
2021-03-31 10:28:17 -0400

    Created dummy demo folder for testing landing page/components/documentation

---
+++ demo copy/client/components/Topic.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { FC } from 'react';
+import { Card, CardContent, Typography, createStyles, makeStyles } from '@material-ui/core';
+
+type Props = {
+    topicInfo: {
+      name: string,
+      partitions: number
+    },
+    id: number,
+}
+
+const Topic: FC<Props> = ({topicInfo, id}: Props) => {
+
+  const useStyles = makeStyles(() => 
+    createStyles({
+      root:  {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: '1rem 1rem 1rem 1rem'
+      },    
+    })
+  );
+
+  const classes = useStyles();
+
+  return(
+    <Card key={id} className={classes.root} >
+      {/* <ListItemText primary={topicInfo.name} secondary={`partitions: ${topicInfo.partitions}`}/> */}
+      <CardContent>
+        <Typography variant='body2' align='center' noWrap={true}>
+          {topicInfo.name}
+        </Typography>
+        <Typography variant='body2' color='textSecondary' noWrap={true}>
+          {`partitions: ${topicInfo.partitions}`}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Topic;

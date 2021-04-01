c4db5b0100af33a4840717059e0cda5aeb52e70d
Ziyad E. El Baz <ziyad.elbaz@me.com>
2021-04-01 09:58:33 -0600

    Created FailFast Error class

--- kafka-penguin/dist/index.js
+++ kafka-penguin/dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
-const failfast = require('./failfast.js');const deadLetterQueueSource = require('./deadLetterQueue.js');
+const failfast = require('./failfast.js');
+const deadLetterQueueSource = require('./deadLetterQueue.js');
 // Export default strategies
 module.exports = {
     failfast,

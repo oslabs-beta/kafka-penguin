82eaf32efc6c3bd9cee80c66bbb019d802dd222b
ausarenglish <ausareenglish@gmail.com>
2021-04-13 12:32:07 -0400

    Created Ignore class producer method

--- kafka-penguin/src/index.ts
+++ kafka-penguin/src/index.ts
@@ -1,5 +1,6 @@
 import { CompressionTypes } from 'kafkajs';
 
+
 /*
 ~~~~~~~~~~~ Dead Letter Queue ~~~~~~~~~~~~~
 */
@@ -199,6 +200,169 @@ export class DeadLetterQueue {
     return adminCreateDLQ;
   }
 }
+/*
+~~~~~~~~~~~ Ignore ~~~~~~~~~~~~~
+*/
+
+export class IgnoreErrorProducer extends Error {
+  message: any;
+  reference: any;
+  name: any;
+  retryCount: number;
+  strategy: string;
+  originalError: any;
+  constructor(e: any) {
+    super(e);
+    Error.captureStackTrace(this, this.constructor)
+    this.strategy = 'Ignore';
+    this.reference = `This error was executed as part of the kafka-penguin Ignore message reprocessing strategy. Your producer attempted to deliver a message ${e.retryCount + 1} times but was unsuccessful.`;
+    this.name = e.name + '(Producer Side)';
+    this.message = e.message;
+    this.originalError = e.originalError;
+    this.retryCount = e.retryCount;
+  }
+}
+
+export class IgnoreErrorConsumer extends Error {
+  message: any;
+  reference: any;
+  name: any;
+  retryCount: number;
+  strategy: string;
+  originalError: any;
+  constructor(e: any) {
+    super(e);
+    Error.captureStackTrace(this, this.constructor)
+    this.strategy = 'Ignore';
+    this.reference = `This error was executed as part of the kafka-penguin Ignore message reprocessing strategy. Your consumer attempted to receive a message ${e.retryCount + 1} times but was unsuccessful. As a result, the message was sent to a Dead Letter Queue. Refer to the original error for further information`;
+    this.name = e.name + '(Consumer Side)';
+    this.message = e.message;
+    this.originalError = e.originalError;
+    this.retryCount = e.retryCount;
+  }
+}
+
+export class Ignore {
+  client: any;
+  topic: string;
+  callback?: (message: any) => boolean;
+  innerConsumer: any;
+  admin: any;
+  innerProducer: any;
+  constructor(client: any, topic: string, callback?: any) {
+    this.topic = topic;
+    this.client = client;
+    this.callback = callback;
+    this.admin = this.client.admin();
+    this.innerConsumer = null;
+    this.innerProducer = this.client.producer();
+  }
+
+
+  producer() {
+    // Reference the Ignore instance for closure in the returned object
+    const ignoreInstance = this;
+    const { innerProducer } = ignoreInstance
+    // Return an object with all Producer methods adapted to execute Ignore strategy
+    console.log('INNER PRODUCER======', innerProducer)
+    return {
+      ...innerProducer,
+      connect() {
+        return innerProducer.connect()
+          .catch((e: Error) => console.log(e))
+      },
+      send(message: messageValue) {
+        return innerProducer.connect()
+          .then(() => {
+            innerProducer.send({
+              ...message,
+              topic: message.topic,
+              messages: message.messages
+            })
+              .catch((e: Error) => {
+                 console.log(e)
+                // Print the error to the console
+                const newError = new IgnoreErrorProducer(e);
+                console.log(newError);
+                })
+            });
+      }
+    }
+  }
+
+  
+  consumer(groupId: { groupId: string }) {
+    this.innerConsumer = this.client.consumer(groupId);
+    const dlqInstance = this;
+    const { innerConsumer, innerProducer } = dlqInstance
+    // Returns an object with all Consumer methods adapter to execute a dead letter queue strategy
+        
+    return {
+      ...innerConsumer,
+      connect() {
+        return innerConsumer.connect().then(() => {
+          dlqInstance.createDLQ();
+        });
+      },
+      subscribe(input?: consumerSubscribeInput) {
+        return innerConsumer.subscribe({
+          ...input, 
+          topic: dlqInstance.topic, 
+          fromBeginning: false 
+        });
+      },
+      run(input: consumerRunInput) {
+        const { eachMessage } = input;
+        return innerConsumer.run({
+          ...input,
+          eachMessage: ({ topic, partitions, message }: { topic: string, partitions: number, message: any }) => {
+            try {
+              //If user doesn't pass in callback, DLQ simply listens and returns errors
+              if (dlqInstance.callback) {
+                if (!dlqInstance.callback(message)) throw Error;
+                eachMessage({ topic, partitions, message });
+              }
+
+            } catch (e) {
+              const newError = new DeadLetterQueueErrorConsumer(e)
+              console.error(newError);
+              innerProducer.connect()
+                .then(() => console.log('kafka-penguin: Connected to DLQ topic'))
+                .then(() => {
+                  innerProducer.send({
+                    topic: `${dlqInstance.topic}.deadLetterQueue`,
+                    messages: [message],
+                  })
+                })
+                .then(() => console.log('kafka-penguin: Message published to DLQ'))
+                .then(() => innerProducer.disconnect())
+                .then(() => console.log('kafka-penguin: Producer disconnected'))
+                .catch((e: any) => console.log('Error with producing to DLQ: ', e));
+            }
+          },
+        });
+      }
+    }
+  }
+  // Creates a new DLQ topic with the original topic name
+  async createDLQ() {
+    const adminCreateDLQ = await this.admin.connect()
+      .then(async () => {
+        await this.admin.createTopics({
+          topics: [{
+            topic: `${this.topic}.deadLetterQueue`,
+            numPartitions: 1,
+            replicationFactor: 1,
+            replicaAssignment: [{ partition: 0, replicas: [0, 1, 2] }],
+          }],
+        });
+      })
+      .then(() => this.admin.disconnect())
+      .catch((err: any) => console.log('Error from createDLQ', err));
+    return adminCreateDLQ;
+  }
+}
+
 /*
 ~~~~~~~~~~~ Fail Fast ~~~~~~~~~~~~~
 */

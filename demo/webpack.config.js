c60aa07b7b086ea5eece7af93a275d93c4593eff
Ausar English <61920215+ausarenglish@users.noreply.github.com>
2021-04-13 20:59:05 -0400

    Feature/ignore strategy (#47)
    * kafka-penguin updated
    
    * latest global updates to kafka-penguin
    
    * deleted demo-copy
    
    * created demo/Dockerfile
    
    * working on fixing ES6 module bug
    
    * reverted webpack proxy
    
    * containerized demo
    
    * april 13 latest pull from main
    
    * Created Ignore class producer method
    
    * created Ignore consumer strategy

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: "production",
   entry: {
     app: ['./client/index.tsx'],
     vendor: ['react', 'react-dom']

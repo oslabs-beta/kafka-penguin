1ef7c3fb84ccad85c8d41d54d94dac58d2ff19fa
ausarenglish <ausareenglish@gmail.com>
2021-04-14 13:36:51 -0400

    latest changes to exampleIgnore files

--- exampleDLQProducer.ts
+++ exampleDLQProducer.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { DeadLetterQueue } from 'kafka-penguin';
+import { DeadLetterQueue } from './index';
 
 const producerClientDLQ = require('./clientConfig.ts');
 
